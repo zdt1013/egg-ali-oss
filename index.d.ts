724bcc9bb9001cdfdc21cf31eba1164f05f79c6c
zdt <zdt1013@qq.com>
2023-04-12 10:05:53 +0800

    添加d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
-import * as OssClient from 'ali-oss';
+import OSS from 'ali-oss';
 
-export { OssClient };
+export { OSS };
 
 declare module 'egg' {
   interface Application {
-    aliOss: { get(id: string): OssClient } & OssClient;
+    aliOss: { get(id: string): OSS } & OSS;
   }
 
   interface Context {
